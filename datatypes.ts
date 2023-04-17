6cf76ef667a09adcbf790961375d05b7bab69581
Chander Bhanu <chanderbhanu096@gmail.com>
2023-04-17 23:40:58 +0200

    type_parsing

--- datatypes.ts
+++ datatypes.ts
@@ -23,8 +23,14 @@ console.log(result)
 //NUMBER 
 // ways to declare a number in typescript
 let age : number;
+//the above age is defined as a number
 age = 10;
 age = 10.4;
+
+// 10 is string here
 let dab = "10";
+console.log(dab);
+//parseInt will convert the string to a number
 let result1 = parseInt(dab);
+console.log(result1);
  
