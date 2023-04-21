588aed2434da32ad310ca4ca232094ea9ddb0c85
Chander Bhanu <chanderbhanu096@gmail.com>
2023-04-21 23:50:33 +0200

    inheritance..

--- class.js
+++ class.js
@@ -1,8 +1,20 @@
 "use strict";
 class Employee {
+    constructor(name, age, salary, address) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.address = address;
+    }
+    getNameWithAddress() {
+        return this.name + " " + this.address;
+    }
 }
-let john = new Employee();
-john.name = "John";
-john.age = 30;
-john.salary = 10000;
+let john = new Employee('ankush', 25, 10000, "Bangalore");
+let address = john.getNameWithAddress();
+// john.name = "John";
+// john.age = 30;
+// john.salary = 10000;
+// john.address = "Bangalore";
 console.log(john);
+console.log(address);
--- class.ts
+++ class.ts
@@ -1,14 +1,48 @@
 class Employee {
+    //properties
+    // private or # make the variable private
     name!: string;
     age!: number;
     salary!: number;
+    protected address!: string;
+    // protected are accessible in child class or within the same class
+
+    //constructor
+    constructor(name: string, age: number, salary: number, address: string) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.address = address;
+    }
+
+    //method
+    getNameWithAddress(): string {
+        return this.name + " " + this.address;
+        //OR return `${this.name} ${this.address}`;
+    }
    
 }
 
-let john = new Employee();
 
-john.name = "John";
-john.age = 30;
-john.salary = 10000;
+
+// Instance of class or object
+let john = new Employee('ankush', 25, 10000, "Bangalore");
+let address = john.getNameWithAddress();
+
+class manager extends Employee {
+    constructor(name: string, age: number, salary: number, address: string) {
+        //super keyword is used to call the constructor of the parent class
+        super(name, age, salary, address);
+    }
+}
+
+
+// john.name = "John";
+// john.age = 30;
+// john.salary = 10000;
+// john.address = "Bangalore";
 
 console.log(john);
+console.log(address);
+
+class 
