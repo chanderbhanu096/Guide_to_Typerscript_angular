366b3be865a2213067e01fc5fd4f73b2c013f3d4
Chander Bhanu <chanderbhanu096@gmail.com>
2023-04-29 23:28:06 +0200

    interfaces

---
+++ interface.ts
@@ -0,0 +1,18 @@
+// Interfaces can be extended by other interfaces.
+// Interface can extend multiple interfaces
+// Interface can extend a class but only the members(public and private) will be inherited not the implementation
+// Interface cannot implement another interface or class
+interface User{
+    // A type alias cannot be extended or implemented from (nor can it extend/implement other types).
+    // Basically interfaces are used to create as a module which can be used by other classes using implement so the same type doesn't have to be written again and again
+    name: string;
+    age?: number;
+    id: number;
+    email: string;
+}
+
+// this
+let user_1: User = {name: "John", age: 25, id: 1, email: ""}
+// OR Both are right because age is optional
+let user2: User = {name: "ABC", id: 1, email: ""}
+
